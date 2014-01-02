7ee7438e942d1c5a26276a6cbacdf22effd384c3
Crt Ahlin <crt.ahlin@gmail.com>
2014-01-02 13:07:10 +0100

    backup. transforming import into a function.

--- AIRS_Data_Import.R
+++ AIRS_Data_Import.R
@@ -1,13 +1,47 @@
 ### Import data from a CSV file exported from the AIRS android app
 
+AIRSImport <- function( filePath, 
+                        sensor
+) {
+  
+  # read data from CSV file
+  data <- readLines(con=filePath)
+  
+  # find lines with patterns that match desired sensor 
+  validEntries <- grep(pattern=sensor,x=data)
+  
+  # read data into dataframe
+  system.time({
+    temp <- gsub(x = data[validEntries], pattern = "#", replacement = "")
+    assign( x=paste0("AIRS_", sensor),
+            value=read.table(text=temp,
+                             sep=";", 
+                             comment.char="@"
+            )
+    )
+    rm(temp)
+    rm(data)
+  })
+  
+  # name the columns of the HP measurements
+  colnames(AIRS_HP) <- c("Miliseconds","Sensor","Value")
+  
+  # add a date column to data
+  date <- vector(length=dim(get(paste0("AIRS_", sensor)))[1], mode="numeric")
+  names(date) <- "Date"
+  date <- get(paste0("AIRS_", sensor))$Miliseconds + as.double(miliseconds)
+  date <- as.POSIXlt(x=date/1000, tz="CET", origin="1970-01-01 00:00.00 CET")
+  assign( x=paste0("AIRS_", sensor),
+          value=cbind(get(paste0("AIRS_", sensor)), date))
+}
+
 # set path to CSV file exported form AIRS
-filePath <- "C:/Users/Crt Ahlin/Documents/Dropbox/DataSets/AIRS/1385030889684.txt"
+filePath <- "C:/Users/Crt Ahlin/Documents/Dropbox/DataSets/AIRS/1385030889684_2.txt"
 
 # set desired sensor
 sensor <- "HP"
 
-# read data from CSV file
-data <- readLines(con=filePath)
+
 
 # extract the timestamp of data export from CSV file name
 # assumptions: the origin is "1970-01-01 00:00.00 CET"
@@ -20,32 +54,27 @@ str(miliseconds)
 fileTimestamp <- as.POSIXlt(x=seconds, tz="CET", origin="1970-01-01 00:00.00 CET")
 fileTimestampMiliseconds <- miliseconds
 
-# find lines with patterns that match desired sensor 
-validEntries <- grep(pattern=sensor,x=data)
+
 
 # list valid entries for chosen sensor
 data[validEntries]
 
-# read data from into dataframe
-system.time({
-assign(x=paste0("AIRS_", sensor), 
-       value=read.table(text=data[validEntries],sep=";", comment.char="@", sep="#"))
 
-})
 
 # name the columns of the HP measurements
 colnames(AIRS_HP) <- c("Miliseconds","Sensor","Value")
 
 
-# add a date column to AIRS_HP
-date <- vector(length=dim(AIRS_HP)[1])
-names(date) <- "Date"
-date <- AIRS_HP$Miliseconds + as.double(miliseconds)
-date <- as.POSIXlt(x=date/1000, tz="CET", origin="1970-01-01 00:00.00 CET")
-AIRS_HP <- cbind(AIRS_HP, date)
+
 
 head(AIRS_HP)
 str(AIRS_HP)
 
 plot(AIRS_HP$Value ~ AIRS_HP$Miliseconds,
      type="l")
+
+# TODO: 
+Naredi export podatkov s telefona
+dodaj, da odstrani # znak iz začetka vrstic
+popravi import začetnega timestampa (nov format)
+zbriši odvečen vrstice, pokomentiraj
