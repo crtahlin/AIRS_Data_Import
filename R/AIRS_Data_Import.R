cfb198be992b1b79be9139443202c0a4ef7404f0
Crt Ahlin <crt.ahlin@gmail.com>
2014-03-24 12:18:29 +0100

    Added a function that returns additional columns (weekdays, etc.)

--- R/AIRS_Data_Import.R
+++ R/AIRS_Data_Import.R
@@ -3,14 +3,14 @@
 #' generated CSV file into R.
 #' 
 #' @details 
-#' Currently implemented specificaly for "HP" aka "heart rate" sensor 
+#' Currently implemented specificaly for "HP" aka "heart pulse" sensor 
 #' (i.e. Zephyr bluetooth). It might in principle work on other sensor data,
 #'  although the columns will surely not get proper names.
 #'  
 #' @param filePath The path to the CSV file containing AIRS exported data.
 #' Use forward slashes to separate folders (e.g. "/").
 #' 
-#' @param sensor The abbreviation representing the sensor. I.e "HP" for heart rate.
+#' @param sensor The abbreviation representing the sensor. I.e "HP" for heart pulse (heart rate).
 #' 
 #' @export 
 AIRSImport <- function( filePath, 
@@ -34,8 +34,7 @@ AIRSImport <- function( filePath,
     assign( x=paste0("AIRS_", sensor),
             value=read.table(text=temp,
                              sep=";", 
-                             comment.char="@" 
-                             )
+                             comment.char="@" )
             )
     rm(temp)
     rm(data)
@@ -45,19 +44,53 @@ AIRSImport <- function( filePath,
   
   # name the columns of the HP measurements
   if (sensor=="HP") {
-    colnames(AIRS_HP) <- c("Miliseconds","Sensor","Value")
+    colnames(AIRS_HP) <- c("MilisecsSinceOrigin","Sensor","Value")
   }
   
   # add a date column to data
   Date <- vector(length=dim(get(paste0("AIRS_", sensor)))[1], mode="numeric")
-  Date <- get(paste0("AIRS_", sensor))$Miliseconds/1000 + initialDate
+  Date <- get(paste0("AIRS_", sensor))$MilisecsSinceOrigin/1000 + initialDate
   assign( x=paste0("AIRS_", sensor),
           value=cbind(get(paste0("AIRS_", sensor)), Date)
-          )
+  )
   rm(Date)
+  # Why did I choose to return AIRS+sensor names specificaly? Just to complicate things?
   return(get(paste0("AIRS_", sensor)))
 }
 
+
+#' @title Import AIRS data exported as CSV from an Android phone and add extra information
+#' @description A function to import data for a certain sensor from and AIRS 
+#' generated CSV file into R. The function adds extra columns to imported data 
+#' for easier data manipulation.
+#' 
+#' @details 
+#' Currently implemented specificaly for "HP" aka "heart pulse" sensor 
+#' (i.e. Zephyr bluetooth). It might in principle work on other sensor data,
+#'  although the columns will surely not get proper names.
+#'  
+#' @param filePath The path to the CSV file containing AIRS exported data.
+#' Use forward slashes to separate folders (e.g. "/").
+#' 
+#' @param sensor The abbreviation representing the sensor. I.e "HP" for heart pulse (heart rate).
+#' 
+#' @export 
+AIRSImportExtended <- function(filePath, sensor) {
+  data <- AIRSImport(
+    filePath=filePath,
+    sensor=sensor)
+  data$TimeinSeconds <- as.POSIXlt(data$Date)[["sec"]] + 
+    as.POSIXlt(data$Date)[["min"]]*60 +
+    as.POSIXlt(data$Date)[["hour"]]*(60*60)
+  
+  # head(data)
+  
+  data$"CyclicTime" <- (data$"TimeinSeconds" / (60*60*24)) %% 1
+  data$"Weekday" <- c("Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", 
+                      "Friday", "Saturday")[as.POSIXlt(data$Date)$wday + 1]
+  return(data)
+  
+}
 
 ########## Below are functions for debugging #############
 # 
